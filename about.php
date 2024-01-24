9476dcac6ae4e5178afc2e73ae652f2bd0982793
Biyuk90 <157506416+Biyuk90@users.noreply.github.com>
2024-01-24 20:36:47 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,126 @@
+<?php
+include 'koneksi.php';
+include 'function.php';
+$menu = 'tentang';
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<?php include 'head.php' ?>
+
+<body class="body">
+
+  <?php include 'navbar.php' ?>
+
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div
+        style="width: 100%;max-width: 100%;max-height: 300px;overflow: hidden; display: flex; justify-content: center; align-items: center;">
+        <img src="assets/img/bg1.jpg" alt="Edot gypsum" width="100%" height="auto">
+        <div class="carousel-caption">
+          <h1><b>TENTANG TOKO BANGUNAN BIFID46</b></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br><br><br>
+
+  <div class="container-custom">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="assets/img/bg1.jpg" alt="Los Angeles" width="100%" height="100%"
+          style="border-radius: 10px 0 0 15px; ">
+      </div>
+      <div class="col-md-6">
+        <div class="container">
+          <br>
+          <h1><b>TENTANG TOKO BANGUNAN BIFID46</b></h1>
+          <br>
+          Selamat datang di TOKO BANGUNAN BIFID46 - Toko Bangunan Terpercaya dan Berkualitas!
+          <br><br>
+          Kami dengan bangga mempersembahkan beragam produk berkualitas tinggi untuk memenuhi segala kebutuhan alat
+          bangunan yang Anda butuhkan. Dengan pengalaman yang kaya dan komitmen terhadap inovasi, TOKO BANGUNAN BIFID46 menjadi pilihan
+          utama bagi mereka yang menghargai estetika dan ketahanan dalam Pembangunan.
+          <br><br>
+          <a href="index.php" class="btn btn-dark btn-lg">LIHAT PRODUK</a>
+          <br><br>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Bahan dan alatk Bangunan Terbaik</h5>
+            <p class="card-text">Dari Besi,kayu,seng,Semen dll ada di sini</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Roster Berkualitas</h5>
+            <p class="card-text">Roster kami tidak hanya memberikan tekstur dan dimensi pada ruang Anda, tetapi juga
+              menambahkan karakter. Dengan pilihan motif yang beragam, kami memungkinkan Anda untuk mengekspresikan gaya
+              Anda dengan penuh kreativitas.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Beton Tahan Lama</h5>
+            <p class="card-text">Keandalan beton kami membuatnya menjadi pilihan tepat untuk proyek interior maupun
+              eksterior. Kami menghadirkan beton dalam berbagai varian, membantu Anda menciptakan lantai dan dinding
+              yang kuat serta indah.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+
+  <div class="container-custom">
+    <div class="container about-us">
+
+      <div class="row">
+        <div class="col-md-6">
+          <div class="row">
+            <div class="card-body">
+              <h4>Kontak kami</h4>
+              <p class="card-text">
+              <p>PT. TOKO BANGUNAN BIFID46</p>
+              Jln.Nazarudin D No1 Kota Pariaman</p>
+              <p>Call/SMS/WA : 089530040415</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="row">
+            <div class="card-body">
+              <h4>Media Sosial</h4>
+              <p class="card-text">
+
+              <p><img src="assets/img/bg1.jpg" width="50px" height="50px"> <a
+                  href="">
+                  BIFID46</a></p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <br><br><br>
+
+  <?php include 'foot.php' ?>
+
+</body>
+
+</html>
---
+++ head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <title>TOKO BANGUNAN BIFID46</title>
+</head>
